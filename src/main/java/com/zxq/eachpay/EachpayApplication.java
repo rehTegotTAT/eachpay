71d345bf88278a727902c7aab606d9a0a60f5fbf
zxq <zxq93245044@163.com>
2022-05-16 17:21:31 +0800

    创建

---
+++ src/main/java/com/zxq/eachpay/EachpayApplication.java
@@ -0,0 +1,13 @@
+package com.zxq.eachpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EachpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EachpayApplication.class, args);
+    }
+
+}
